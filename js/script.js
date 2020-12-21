fbb957fa7d0170fc227bbd696bfbd20daf972407
Ageesa <71363552+Ageesa@users.noreply.github.com>
2020-12-20 22:54:43 -0500

    Added images to popup player

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let chinaMusic= document.getElementById(`cnflg`)
 let brazilMusic= document.getElementById(`brflg`)
 let player = document.querySelector(`.popup`)
 let pausebtn = document.getElementById(`ps`)
-
+let elePics = document.querySelector(`.destinations`)
 
 let sfxm = new Audio(`audio/Mexico.mp3`)// Audio files for each country
 let sfxi = new Audio(`audio/India.mp3`)
@@ -24,6 +24,7 @@ let pauseAll = function(){
     sfxt.pause()
     sfxc.pause()
     sfxb.pause()
+    player.style.display="none";
 }
 
 // Function to Play a music for a country 
@@ -34,7 +35,8 @@ let playMex = function() { //Music for Mexico
         sfxt.pause()
         sfxc.pause()
         sfxb.pause()
-
+        elePics.src="images/mexico.jpg";
+        player.style.display="block";
 }
 
 let playInd = function() { //Music for India
@@ -44,6 +46,8 @@ let playInd = function() { //Music for India
     sfxc.pause()
     sfxb.pause()
     sfxi.play()
+    elePics.src="images/india.jpg";
+    player.style.display="block";
 }
 
 let playSpn = function() { //Music for Spain
@@ -53,6 +57,8 @@ sfxt.play()
 sfxc.pause()
 sfxb.pause()
  sfxs.play()
+ elePics.src="images/spain.jpg";
+ player.style.display="block";
 }
 let playTnz = function() { //Music for Tanzania
 sfxm.pause()
@@ -61,6 +67,8 @@ sfxs.pause()
 sfxt.play()
 sfxc.pause()
 sfxb.pause()
+elePics.src="images/africa.jpg";
+player.style.display="block";
 }
 
 let playCn = function() { // Music for China
@@ -70,6 +78,8 @@ sfxs.pause()
 sfxt.pause()
 sfxc.play()
 sfxb.pause()
+elePics.src="images/china.jpg";
+player.style.display="block";
 }
 
 let playBl = function() { // Music for Brazil
@@ -79,6 +89,8 @@ sfxs.pause()
 sfxt.pause()
 sfxc.pause()
 sfxb.play()
+elePics.src="images/samba.jpg";
+player.style.display="block";
  
 }
 
